1c2476f06f64f67088bb782a2c0f8d32f0ec21a8
Tuyen Nguyen <tuyendev@gmail.com>
2022-07-12 15:56:04 +0700

    fix menubar don't remember last choice

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -1,1 +1,21 @@
-window.addEventListener("DOMContentLoaded",b=>{let a=document.body.querySelector("#sidebarToggle");a&&(null!=localStorage.getItem("sb|sidebar-toggle")&&("false"===localStorage.getItem("sb|sidebar-toggle")?(document.body.classList.remove("sb-sidenav-toggled"),document.body.classList.add("sidebar-collapse")):(document.body.classList.add("sb-sidenav-toggled"),document.body.classList.remove("sidebar-collapse"))),a.addEventListener("click",a=>{a.preventDefault(),document.body.classList.toggle("sb-sidenav-toggled"),localStorage.setItem("sb|sidebar-toggle",document.body.classList.contains("sb-sidenav-toggled"))}))})
+window.addEventListener('DOMContentLoaded', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+
+        if(localStorage.getItem('sb|sidebar-toggle') != null) {
+            if (localStorage.getItem('sb|sidebar-toggle') === 'false') {
+                document.body.classList.remove("sb-sidenav-toggled")
+                document.body.classList.add("sidebar-collapse")
+            } else {
+                document.body.classList.add("sb-sidenav-toggled")
+                document.body.classList.remove("sidebar-collapse")
+            }
+        }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
